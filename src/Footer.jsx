ec3932adc437754f4d06020457ace373c77a8050
Simran Ahuja <125957972+simranahuja13@users.noreply.github.com>
2023-09-20 22:40:24 +0530

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='fixed bottom-5 pt-3 text-2xl font-bold  text-white'>Created with 💌 by <span className=' text-3xl font-extrabold italic bg text-red-700'> Simran Ahuja</span>  </div>
+    <div className='px-5 fixed bottom-5 pt-3 text-2xl font-bold  text-white'>Created with 💌 by <span className=' text-3xl font-extrabold italic bg text-red-700'> Simran Ahuja</span>  </div>
   )
 }
 

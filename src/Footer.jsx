36c8d31ee23dc5793160c13cd514d89ecf070fa4
Simran <officialsimranahuja@gmail.com>
2023-09-20 23:13:29 +0530

    modified Footer.jsx file

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='flex flex-wrap mb-2 text-center px-5 fixed bottom-0 pt-3 text-2xl font-bold  text-white'>Created with 💌 by <span className=' text-3xl font-extrabold italic bg text-red-700'> Simran Ahuja</span>  </div>
+    <div className='flex flex-wrap mb-2 ml-20  text-center fixed bottom-0  text-2xl font-bold  text-white'>Created with 💌 by <span className=' px-2 text-3xl font-extrabold italic bg text-red-700'> Simran Ahuja </span> 
+    </div>
   )
 }
 
